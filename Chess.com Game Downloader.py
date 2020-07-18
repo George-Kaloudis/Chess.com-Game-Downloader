7accc0fe0fd459f6cb85011f5a3252268c0fc37d
George-Kaloudis <georgekal123@gmail.com>
2020-07-18 20:13:12 +0300

    Polished up the code.

--- Chess.com Game Downloader.py
+++ Chess.com Game Downloader.py
@@ -5,9 +5,8 @@ import requests
 
 def main():
     user = input("Player's Name:")
-    where = input("Directory(Empty for current):")
-    cd = date.today()
-    where = os.path.join(where, "%s's Archive %s" % (user, cd))
+    where = os.path.join(input("Directory(Empty for current):"), "%s's Archive %s" % (user, date.today()))
+    
     if not os.path.exists(where + ".pgn"):
         where += ".pgn"
         open(where, 'w').close()
@@ -18,11 +17,13 @@ def main():
             i+=1
         where = where + "(%d).pgn" % (i)
         open(where, 'w').close()
+        
     print("Downloading %s's games to %s:" % (user, where))
     for archive in get('https://api.chess.com/pub/player/%s/games/archives' % user)['archives']:
-        download_archive(archive, where)
+        aa(archive, where)
+    
 
-def download_archive(url, where):
+def aa(url, where):
     games = get(url)['games']
     with open(where, 'a+', encoding='utf-8') as output:
         for game in games:
@@ -31,7 +32,5 @@ def download_archive(url, where):
 
 def get(url):
     return requests.get(url).json()
-
-
 
 main()
